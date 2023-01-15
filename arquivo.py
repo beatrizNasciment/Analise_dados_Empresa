28b6ddbb09cb5d8745f46c2dfc38b1539d80b2b4
fatinha404 <bia.nascimento77733@gmail.com>
2023-01-15 19:52:35 -0300

    colocando os arquivos

---
+++ arquivo.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import plotly.express as px
+
+tabela = pd.read_csv("telecom_users.csv")
+
+tabela = tabela.drop("Unnamed: 0", axis=1) #excluir algo na tabela, se o eixo for == 0 é uma linha se for == 1 é uma coluna
+print(tabela)
+
+# - Valores que estão reconhecidos de forma errada
+tabela["TotalGasto"] = pd.to_numeric(tabela["TotalGasto"], errors="coerce")
+
+# - Valores vazios
+# deletando as colunas vazias
+# axis = 0 _> linha ou axis = 1 _> coluna
+tabela = tabela.dropna(how="all", axis=1)
+# deletando as linhas vazias
+tabela = tabela.dropna(how="any", axis=0)
+
+print(tabela.info())
+
+# Como estão os nossos cancelamentos?
+print(tabela["Churn"].value_counts())
+print(tabela["Churn"].value_counts(normalize=True).map("{:.1%}".format))
+
+# comparar cada coluna da minha tabela com a coluna de cancelamento
+
+for coluna in tabela.columns:
+    # para edições nos gráficos: https://plotly.com/python/histograms/
+    # para mudar a cor do gráfico , color_discrete_sequence=["blue", "green"]
+    grafico = px.histogram(tabela, x=coluna, color="Churn", text_auto=True)
+    # etapa 2: exibir o gráfico
+    grafico.show()
